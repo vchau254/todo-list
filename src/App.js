0740d59772f235c1fe9c25528f137794e5d373a1
Cristian Florea <florecra@novartis.com>
2020-08-04 20:44:35 +0200

    removed unused code

--- src/App.js
+++ src/App.js
@@ -4,19 +4,6 @@ import './App.css';
 import TodoList from './components/TodoList';
 
 class App extends Component {
-  state = { count: 0 };
-
-  increment = () => {
-    this.setState({
-      count: this.setState.count + 1,
-    });
-  };
-
-  decrement = () => {
-    this.setState({
-      count: this.setState.count - 1,
-    });
-  };
   render() {
     return (
       <div className="App">
